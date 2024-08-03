ac91e341b009c7440c845b00b9cd0e4fca87d4ed
Justin Yu <justinyu6866@gmail.com>
2024-08-03 14:48:59 -0400

    Day 1 - Removed reverse column map dictionary because can simply check values with orginal dictionary keys without need for reversing the dictionary. Added method to convert square name to coordinate point. Added a method to convert coordinate point to square name. Replaced the break statement with a repeat flag to make the while loop more readable.

--- BNK/BNK.py
+++ BNK/BNK.py
@@ -9,19 +9,22 @@ knight = "knight"
 bishop = "bishop"
 king = "king"
 ColumnLetters = "abcdefgh"
+letterDictionary = {}#static dictionary variable that stores map of column letters to column numbers
 
 class ChessPiece:
     def __init__(self, pieceName):
         self.PieceName = pieceName
-        self.point = self.getInput(pieceName)
+        self.currentPoint = self.getInput(pieceName)
 
     def getInput(self,pieceName):#uses the piece name for the prompt, otherwise same for all new pieces. Asks for a position ie.a1 and returns corresponding coordinate ie.[1,1]
         #loop will repeat when repeat is true
         repeat = True
-        while (repeat):
+        
+        while (repeat==True):
             #get user input
             entry = input("What square is your %s on (ie. a1)? \n"%(pieceName)).rstrip().lstrip().lower() 
-            repeat = False
+            repeat = False #switch repeat on when an invalid entry is found
+
             if (len(entry)!=2): #condition 1: entry is not two characters long
                  repeat = True
             else:
@@ -33,7 +36,7 @@ class ChessPiece:
                     except:
                         repeat=True
 
-                    if (repeat):
+                    if (repeat == False):
                         if (int(entry[1])>max and int(entry[1])<min):#condition 4: 2nd character is not in range 1-8
                             repeat=True
             if (repeat == True):
@@ -41,15 +44,17 @@ class ChessPiece:
         #end while loop
         return self.convertToPoint(entry)
     
-    def convertToPoint(self, squareName):
+    def convertToPoint(self, squareName):#can convert any square name to a coordinate point ie. a1 is (1,1)
         point = [None]*2
-        #convert a square to a cartesian point ie. a1 is (1,1)
-        point[0] = LetterDictionary[squareName[0]]
+        point[0] = letterDictionary[squareName[0]]
         point[1] = int(squareName[1])
         return point
     
-    def printPoint(self):
-        print(self.point)
+    def convertToName(self, point): #can convert any point to a square name
+        for l in letterDictionary:
+            if (letterDictionary[l]==point[0]):
+                return (l + str(point[1]))
+        
     
 class King(ChessPiece):
     def __init__(self):
@@ -67,14 +72,14 @@ class Bishop(ChessPiece):
 #populate the dictionary for easy future mapping
 i = 1
 for c in ColumnLetters:
-    LetterDictionary[c] = i
+    letterDictionary[c] = i
     i+=1
 
 K = King()
 N = Knight()
 B = Bishop()
-K.printPoint()
-N.printPoint()
-B.printPoint()
+print(K.convertToName(K.currentPoint))
+print(N.convertToName(N.currentPoint))
+print(B.convertToName(B.currentPoint))
 
 
