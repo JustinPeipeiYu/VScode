dea63d44d71904419399f224c8ff872c3375f091
Justin Yu <justinyu6866@gmail.com>
2024-08-03 14:47:59 -0400

    Day 1 - Removed reverse column map dictionary because can simply check values with orginal dictionary keys without need for reversing the dictionary. Added method to convert square name to coordinate point. Added a method to convert coordinate point to square name. Replaced the break statement with a repeat flag to make the while loop more readable.

--- BNK/BNK.py
+++ BNK/BNK.py
@@ -5,67 +5,76 @@
 
 min = 1
 max = 8
-columnMap = {"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8} #letters are keys, numbers are values
-reverseColumnMap = {}
 knight = "knight"
 bishop = "bishop"
 king = "king"
+ColumnLetters = "abcdefgh"
 
 class ChessPiece:
-    def __init__(self, name):
-        self.square = self.validateInput(name)
+    def __init__(self, pieceName):
+        self.PieceName = pieceName
+        self.point = self.getInput(pieceName)
 
-    def validateInput(self,name):
-        while (True):
-            #flag to decide if the user's entry is valid when the while loop ends
-            valid = True 
-        
+    def getInput(self,pieceName):#uses the piece name for the prompt, otherwise same for all new pieces. Asks for a position ie.a1 and returns corresponding coordinate ie.[1,1]
+        #loop will repeat when repeat is true
+        repeat = True
+        while (repeat):
             #get user input
-            entry = input("What square is your %s on (ie. a1)? \n"%(name)).rstrip().lstrip().lower() 
-
-            if (len(entry)!=2): #condition 1: entry is two characters long
-                 valid = False
+            entry = input("What square is your %s on (ie. a1)? \n"%(pieceName)).rstrip().lstrip().lower() 
+            repeat = False
+            if (len(entry)!=2): #condition 1: entry is not two characters long
+                 repeat = True
             else:
-                if (entry[0] not in columnMap): #condition 2: 1st character is letter a-h
-                    valid = False 
+                if (entry[0] not in ColumnLetters): #condition 2: 1st character is not letter a-h
+                    repeat = True 
                 else:
-                    try: #condition 3: 2nd character is integer
+                    try: #condition 3: 2nd character is not integer
                         int(entry[1])
                     except:
-                        valid=False
-                    if (valid):
-                        if (int(entry[1])>max and int(entry[1])<min):#condition 4: 2nd character is in range 1-8
-                            valid=False
+                        repeat=True
 
-            if (valid): #all checkpoints passed, break while loop, return value
-                break
-            else:
+                    if (repeat):
+                        if (int(entry[1])>max and int(entry[1])<min):#condition 4: 2nd character is not in range 1-8
+                            repeat=True
+            if (repeat == True):
                 print("Sorry that is an invalid entry. Please try again.")
-                continue 
-        return entry    
-            
+        #end while loop
+        return self.convertToPoint(entry)
+    
+    def convertToPoint(self, squareName):
+        point = [None]*2
+        #convert a square to a cartesian point ie. a1 is (1,1)
+        point[0] = LetterDictionary[squareName[0]]
+        point[1] = int(squareName[1])
+        return point
+    
+    def printPoint(self):
+        print(self.point)
+    
 class King(ChessPiece):
-    name = knight
-    def __init__(self, name):
-        ChessPiece.__init__(self, name)
+    def __init__(self):
+        ChessPiece.__init__(self, king)
 
 class Knight(ChessPiece):
-    name = knight
-    def __init__(self, name):
-        ChessPiece.__init__(self, name)
-        
+    def __init__(self):
+        ChessPiece.__init__(self, knight)
         
 class Bishop(ChessPiece):
-    name = knight
-    def __init__(self, name):
-        ChessPiece.__init__(self, name)
+    def __init__(self):
+        ChessPiece.__init__(self, bishop)
 
 #Main Program
-K = King(king)
-N = Knight(knight)
-B = Bishop(bishop)
+#populate the dictionary for easy future mapping
+i = 1
+for c in ColumnLetters:
+    LetterDictionary[c] = i
+    i+=1
 
-#create reverse column mapping (numbers are keys, letters are values)
-for c in columnMap:
-    reverseColumnMap[columnMap[c]] = c
+K = King()
+N = Knight()
+B = Bishop()
+K.printPoint()
+N.printPoint()
+B.printPoint()
+
 
