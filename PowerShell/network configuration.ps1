e540fac6d699ee7ef31fe2e045a7309d529cfdf4
Justin Yu <justinyu6866@gmail.com>
2024-09-26 14:13:23 -0400

    network configuration

---
+++ PowerShell/network configuration.ps1
@@ -0,0 +1,9 @@
+New-NetIPAddress 
+-InterfaceAlias Ethernet0 
+-AddressFamily IPv4 
+-IPAddress 10.180.117.8 
+-PrefixLength 24 
+-DefaultGateway 10.180.117.1
+Set-DnsClientServerAddress
+-InterfaceIndex 4
+-ServerAddresses 10.180.117.7
