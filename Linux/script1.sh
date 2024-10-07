f51a4a13da5352662d434e144c676bf938724d2a
Justin Yu <justinyu6866@gmail.com>
2024-10-07 01:15:48 -0400

    first shell script

---
+++ Linux/script1.sh
@@ -0,0 +1,145 @@
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+
+nmtui-edit                           zipgrep
+ nmtui-hostname                       zipinfo
+ nohup                                zipnote
+ nproc                                zipsplit
+ nroff                                zless
+ nsenter                              zmore
+ numfmt                               znew
+ od                                   zsoelim
+[root@RockyLinux3 bin]# vim script1.sh
+#! /bin/bash
+mkdir /home/justin
+cp ./../home/yu/labfiles/nursery ./../home/yu/labfiles/sonnet  ./../home/justin
+mkdir ./../home/justin/files1; mkdir ./../home/justin/files1/sonnets; mkdir ./../home/justin/files1/rhymes; mkdir ./../home/justin/files1/combos
+touch poems; cat /home/justin/files1/sonnets/sonnet > poems; cat /home/justin/files1/rhymes/nursery >> poems 
+mv /home/justin/labfiles/sonnet /home/justin/files1/sonnets/sonnet; mv /home/justin/labfiles/nursery /home/justin/files1/rhymes/nursery; mv ./poems /home/justin/files1/combos/poems
+cd /
+cd home
+cd yu
+cp -r ./../justin ./../ahmed 
+cd /home/justin
+cp -r files1 ./files2
+cd /home/justin/files1/sonnets
+cp sonnet bonnet
+cp sonnet donnet
+exit 0
