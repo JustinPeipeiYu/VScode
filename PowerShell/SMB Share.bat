fb7bd6aa977f5bd5fb6febdf683660416649618a
Justin Yu <justinyu6866@gmail.com>
2024-10-31 20:55:55 -0400

    changes to smb share

--- PowerShell/SMB Share.bat
+++ PowerShell/SMB Share.bat
@@ -1,1 +1,2 @@
-net share DataShare=S:\Data1 /grant:Everyone,full /users:50 /unlimited /remark:"" /cache:none
+net share DataShare=S:\Data1 /grant:Everyone,full /remark:"This is a SMB share" 
+net share DataShare /delete
